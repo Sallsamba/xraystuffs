c3fde8de07a74f46808479426d7234e7d1c142ec
Samba <samba.sall@student-cs.fr>
2019-11-20 15:23:37 +0100

    modify database folder name

--- recognition/add_data.py
+++ recognition/add_data.py
@@ -3,15 +3,15 @@ import shutil
 
 def add_to_database(path,theclass,subclass):
 
-    if theclass in os.listdir("P" ):
-        if subclass in os.listdir("P/"+ theclass):
-            shutil.copy2(path, "P/"+theclass +"/"+ subclass +"/"+ path)
+    if theclass in os.listdir("database" ):
+        if subclass in os.listdir("database/"+ theclass):
+            shutil.copy2(path, "database/"+theclass +"/"+ subclass +"/"+ path)
         else:
-            os.makedirs("P/"+theclass + "/" + subclass)
-            shutil.copy2(path, "P/"+theclass +"/"+ subclass +"/"+ path)
+            os.makedirs("database/"+theclass + "/" + subclass)
+            shutil.copy2(path, "database/"+theclass +"/"+ subclass +"/"+ path)
 
 
     else:
-        os.makedirs("P/"+theclass)
-        os.makedirs("P/"+theclass + "/" + subclass)
-        shutil.copy2(path, "P/"+theclass +"/"+ subclass +"/"+ path)
+        os.makedirs("database/"+theclass)
+        os.makedirs("database/"+theclass + "/" + subclass)
+        shutil.copy2(path, "database/"+theclass +"/"+ subclass +"/"+ path)
